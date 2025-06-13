55e0f10333579f2005cd0b96c2b3b6679f77f9ac
Jake <boykjgis@gmail.com>
2025-06-13 15:57:41 -0600

    Navbar.jsx: Add home class for navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,11 @@ import './Navbar.css';
 const Navbar = () => {
 	return (
 		<nav className = "navbar">
-
+			<div className = "navbar-left">
+				<a href = "/" className="logo">
+					Home
+				</a>
+			</div>
 		</nav>
 	);
 };
